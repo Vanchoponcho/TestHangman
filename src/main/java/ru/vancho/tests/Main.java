9ab405d7a9f3d215b9ad91aa43e98e5c201de24c
i.ogurtsov <floomerka@gmail.com>
2024-05-15 00:36:36 +0300

    change Main

--- src/main/java/ru/vancho/tests/Main.java
+++ src/main/java/ru/vancho/tests/Main.java
@@ -2,6 +2,6 @@ package ru.vancho.tests;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello from Hangman!");
     }
 }
